01853bf5c1503b27b4a0fb6caec3b3b57457de9c
artem <89ufisum@gmail.com>
2025-08-31 18:08:38 +0300

    f4

--- server/index.js
+++ server/index.js
@@ -11,18 +11,24 @@ import OrdersBook from './ws/ordersBook.js';
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
-// app.use(cors({ origin: 'https://screener-vite.vercel.app'}));
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     const allowedOrigins = ['http://localhost:5173', 'https://screener-vite.vercel.app'];
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 
 app.use(cors({
-  origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:5173', 'https://screener-vite.vercel.app'];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
+  origin: ['https://screener-vite.vercel.app'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // если нужны куки или авторизация
 }));
+
 
 app.use('/api', futures);
 app.use('/api', klines);
