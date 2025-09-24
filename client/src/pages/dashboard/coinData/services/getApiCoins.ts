ca4dca30b9dfdbd103354e61c1c3958e1b308d25
artem <89ufisum@gmail.com>
2025-09-24 20:16:56 +0300

    r2

--- client/src/pages/dashboard/coinData/services/getApiCoins.ts
+++ client/src/pages/dashboard/coinData/services/getApiCoins.ts
@@ -5,8 +5,8 @@ import { CurrentPrice, OrdersBookResponse } from '@/pages/ordersBookPage/types';
 
 export const coinsApi = createApi({
     reducerPath: 'coinsApi',
-    // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000'}),
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://my-server-latest-1.onrender.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000'}),
+    // baseQuery: fetchBaseQuery({ baseUrl: 'https://my-server-latest-1.onrender.com' }),
     endpoints: (builder) => ({
         getCoins: builder.query<OriginalResponse, void>({
             query: () => '/api/get-futures',
--- client/src/pages/dashboard/types/tools.d.ts
+++ client/src/pages/dashboard/types/tools.d.ts
@@ -1,19 +1,37 @@
+// import { IChartApi } from 'lightweight-charts';
+
+// export {};
+
+// declare global {
+//   interface Window {
+//     LightweightCharts: {
+//       createChartTools: (...args: any[]) => ExtendedChartApi;
+//       version?: string;
+//     };
+//   }
+
+//   interface ExtendedChartApi extends IChartApi {
+//     addLineTool: (toolName: string, points: any[], options: any) => void;
+//     _private__chartWidget: {
+//     addSeries: (seriesType: any, options?: any) => any;
+//   };
+//   }
+// }
+
 import { IChartApi } from 'lightweight-charts';
-
 export {};
-
 declare global {
   interface Window {
-    LightweightCharts: {
+    LightweightCharts: typeof LightweightCharts & {
       createChartTools: (...args: any[]) => ExtendedChartApi;
       version?: string;
     };
   }
+}
 
-  interface ExtendedChartApi extends IChartApi {
-    addLineTool: (toolName: string, points: any[], options: any) => void;
-    _private__chartWidget: {
+interface ExtendedChartApi extends IChartApi {
+  addLineTool: (toolName: string, points: any[], options: any) => void;
+  _private__chartWidget: {
     addSeries: (seriesType: any, options?: any) => any;
   };
-  }
 }
